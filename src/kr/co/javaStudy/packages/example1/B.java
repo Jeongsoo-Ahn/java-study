6618118a8d6b589cd05b8ca419a04a9fadd65b23
mynameisajs <mynameisajs@hotmail.com>
2015-10-25 19:14:25 +0900

    Added

---
+++ src/kr/co/javaStudy/packages/example1/B.java
@@ -0,0 +1,9 @@
+package kr.co.javaStudy.packages.example1;
+
+public class B {
+
+	public static void main(String[] args) {
+		A a = new A();
+	}
+
+}
---
+++ src/kr/co/javaStudy/packages/example2/C.java
@@ -0,0 +1,12 @@
+package kr.co.javaStudy.packages.example2;
+import kr.co.javaStudy.packages.example1.*;
+
+public class C {
+
+	public static void main(String[] args) {
+		
+		A a = new A();
+
+	}
+
+}
