88a30aad06796c10b4f4ebf05dbaad0313d521df
안정수 <mynameisajs@ticketlink.co.kr>
2016-03-18 17:40:39 +0900

    feature test commit

--- src/kr/co/javaSrudy/accessmodifier/CalculatorDemo.java
+++ src/kr/co/javaSrudy/accessmodifier/CalculatorDemo.java
@@ -21,9 +21,7 @@ class Calculator{
 public class CalculatorDemo {
     public static void main(String[] args) {        
         Calculator c1 = new Calculator();
-        c1.setOprands(10, 20);
+        c1.setOprands(20, 30);
         c1.sumDecoPlus();
-        c1.sumDecoMinus();
-
     }
 }
